2cff14e7888f56557f63bc6ade70632bd5c335a9
vladislav.golik <vladislav.golik@gmail.com>
2023-09-13 19:19:57 +0300

    hotfix

--- src/split_96_to_32.py
+++ src/split_96_to_32.py
@@ -1,9 +1,19 @@
 import argparse
 
+MASK_32BIT = 0xFFFFFFFF  # 32-битная маска
+
 def split_96_to_32(num):
-    low = num & 0xFFFFFFFF
-    mid = (num >> 32) & 0xFFFFFFFF
-    high = (num >> 64) & 0xFFFFFFFF
+    """
+    Разбивает 96-битное число на три 32-битных числа.
+    :param num: 96-битное число
+    :return: кортеж из трех 32-битных чисел (high, mid, low)
+    """
+    if num >= (1 << 96):
+        raise ValueError("Число больше 96 бит")
+
+    low = num & MASK_32BIT
+    mid = (num >> 32) & MASK_32BIT
+    high = (num >> 64) & MASK_32BIT
 
     return high, mid, low
 
@@ -13,17 +23,19 @@ def main():
 
     args = parser.parse_args()
 
-    num_96bit = args.num
+    try:
+        high, mid, low = split_96_to_32(args.num)
+        print("96-битное число: {}".format(args.num))
+        print("Старшие 32 бита: {}".format(high))
+        print("Средние 32 бита: {}".format(mid))
+        print("Младшие 32 бита: {}".format(low))
+        print("hex")
+        print("Старшие 32 бита: {}".format(hex(high)))
+        print("Средние 32 бита: {}".format(hex(mid)))
+        print("Младшие 32 бита: {}".format(hex(low)))
 
-    high, mid, low = split_96_to_32(num_96bit)
-    print("96-битное число: {}".format(num_96bit))
-    print("Старшие 32 бита: {}".format(high))
-    print("Средние 32 бита: {}".format(mid))
-    print("Младшие 32 бита: {}".format(low))
-    print("hex")
-    print("Старшие 32 бита: {}".format(hex(high)))
-    print("Средние 32 бита: {}".format(hex(mid)))
-    print("Младшие 32 бита: {}".format(hex(low)))
+    except ValueError as e:
+        print(e)
 
 if __name__ == "__main__":
     main()
